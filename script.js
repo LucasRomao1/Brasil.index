736fc791894bad2d331f014441010968f1f526cd
LucasRomao1 <160027793+LucasRomao1@users.noreply.github.com>
2024-04-17 10:53:20 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minha Página</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<nav>
+    <ul>
+        <li><a href="#" onclick="BRASIL É TOP('Brasil')">Brasil</a></li>
+        <li><a href="#" onclick="mostrarMensagem('Brasa')">Brasa</a></li>
+        <li><a href="#" onclick="mostrarMensagem('Brazuka')">Brazuka</a></li>
+        <li><a href="#" onclick="mostrarMensagem('BraBra')">BraBra</a></li>
+    </ul>
+</nav>
+
+<h1> bandeira do Brasil</h1>
+<p>Este é a bandeira do  Brasil.</p>
+
+<script>
+    function mostrarMensagem(item) {
+        alert("Você clicou em " + item);
+        // Aqui você pode adicionar qualquer ação que desejar para cada item do menu
+        hahahhahahah
+    }
+</script>
+
+</body>
+</html>
